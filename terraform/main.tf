f77f3c916fb76eec8d2ff6642db751a0ba5723b6
Luciano Sampaio <lsampaioweb@gmail.com>
2023-02-14 00:17:40 -0300

    First commit

---
+++ terraform/main.tf
@@ -0,0 +1,34 @@
+module "homelab_project" {
+  source  = "lsampaioweb/homelab-project/proxmox"
+  version = "1.0.1"
+
+  project     = var.project
+  environment = var.environment
+
+  vm_instance = var.vm_instance
+}
+
+module "dynamic_inventory" {
+  source  = "lsampaioweb/dynamic-inventory/local"
+  version = "1.0.0"
+
+  hosts_list = [
+    for key, value in var.vm_instance :
+    {
+      hostname    = module.homelab_project.vms[key].vm_name
+      public_ip   = module.homelab_project.vms[key].vm_ipv4
+      password_id = module.homelab_project.vms[key].vm_cloned_from
+
+      state          = value.state
+      priority       = value.priority
+      unicast_src_ip = module.homelab_project.vms[key].vm_ipv4
+
+      unicast_peer_ip = join(",", [
+        for key1, value1 in var.vm_instance :
+        module.homelab_project.vms[key1].vm_ipv4
+        if module.homelab_project.vms[key].vm_ipv4 !=
+        module.homelab_project.vms[key1].vm_ipv4
+      ])
+    }
+  ]
+}
